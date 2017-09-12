bf38e7e2744aab4e9052cd1706d2fb28667d0ec5
Sarthak Srivastava <sarth1597@gmail.com>
2017-09-13 02:26:28 +0530

    image reading

---
+++ abc.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img = cv2.imread('Untitled.jpg',cv2.IMREAD_GRAYSCALE)
+#IMREAD_GRAYSCALE=0
+#IMREAD_COLOR=1
+#IMREAD_UNCHANGED= -1
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+plt.imshow(img,cmap = 'gray',interpolation ='bicubic')
+plt.show()
